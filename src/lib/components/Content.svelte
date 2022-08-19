6f91958e991e22d157f8c3c968682f40268cf2a8
Coordinate-Cat <tetrapasta02@gmail.com>
2022-08-19 21:54:37 +0900

    feat: style

--- src/lib/components/Content.svelte
+++ src/lib/components/Content.svelte
@@ -11,4 +11,17 @@
     overflow: scroll;
     height: 89vh;
   }
+  /*スマホサイズに適用*/
+  @media screen and (max-width:599px){
+    .content {
+      /* max-width: 760px; */
+      /* padding: 1rem; */
+      margin: 4px;
+      border: 1px solid #000;
+      /* margin-top: 72px; */
+      /* margin-bottom: 72px; */
+      overflow: scroll;
+      height: 86vh;
+    }
+  }
 </style>
